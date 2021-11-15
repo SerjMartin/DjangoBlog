961e16a3a04293056749418f1aa2e108531409f2
SerjMartin <serjmartin@mail.com>
2021-11-15 20:35:57 +0000

    Add template and ccs style

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'  # the vew will read index.html
+    paginate_by = 6  # will add only 6 post by page
