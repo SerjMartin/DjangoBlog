8c63ca1c0019d38b4000d93c802d0329f892b15d
SerjMartin <serjmartin@mail.com>
2021-11-16 19:06:11 +0000

    Add post_details file

--- blog/views.py
+++ blog/views.py
@@ -6,8 +6,8 @@ from .models import Post
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'  # the vew will read index.html
-    paginate_by = 6  # will add only 6 post by page
+    template_name = 'index.html'
+    paginate_by = 6
 
 
 class PostDetail(View):
@@ -17,7 +17,7 @@ class PostDetail(View):
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by('created_on')
         liked = False
-        if post.likes.filter(id=self.request.user.id).exist():
+        if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
         return render(
